c44427e3ae2aad44c365110f44adfe8021e9b23a
HaEun <112637518+yellowgree@users.noreply.github.com>
2025-12-09 11:46:46 +0900

    Update2

--- llm_parser/adapter/__init__.py
+++ llm_parser/adapter/__init__.py
@@ -6,10 +6,4 @@ LLM Adapter - adapter package
 from .chatgpt_file_handler import ChatGPTFileHandler
 from .claude_file_handler import ClaudeFileHandler
 
-<<<<<<< HEAD
 __all__ = ['ChatGPTFileHandler', 'ClaudeFileHandler']
-=======
-__all__ = ['ChatGPTFileHandler']
-__all__ = ['ChatGPTFileHandler', 'ClaudeFileHandler']
-
->>>>>>> 0b457e9c9c0b7ae53c762b02ded5f70bb61ed949
